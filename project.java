b896d3ea8778385cb00fb2b22a707201f743221d
pavorn2003 <pavorn2003@gmail.com>
2023-02-03 23:22:47 +0700

    Updated with classes

--- project.java
+++ project.java
@@ -4,11 +4,48 @@ package Project1;
 import java.util.*;
 import java.io.*;
 
+
 public class project {
+    public class product {
+        protected String name;
+        protected int price;
+        protected int weight;
+        protected int totSale = 0;
+        protected int unitSold = 0;
+        public product(String n,int p,int w) {
+            name = n;
+            price = p;
+            weight = w;
+        }
+        public void addSale(int n) {totSale+=n;}
+        public void addUnit(int n) {unitSold+=n;}
+    }
+    public class Customer {
+        protected String name;
+        protected int cashback = 0;
+        public Customer(String n) {name = n;}
+        public void setCashback(int n) {cashback =n;} 
+    }
+    public class Order extends Customer {
+        protected int id;
+        protected String shippingType = "S";
+        public Order(String n,int i,String s) {
+            super(n);
+            id = i;
+            shippingType=s;
+        }
+    }
+    public class ShippingCalculator {
+        //leave for later
+    }
     
     public static void main(String[] args) {
-        String infile = "src/main/Java/project1/orders.txt";
-        Scanner rd = new Scanner(infile);
+        String product = "src/main/Java/project1/products.txt";
+        Scanner rd = new Scanner(product);
+        while(rd.hasNext()) {
+            
+        }
+        
     }
     
 }
