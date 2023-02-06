70d7ca7a0a44dbb444e02e59ff76bf7679427557
pavorn2003 <pavorn2003@gmail.com>
2023-02-06 13:01:18 +0700

    -updated Scanner to catch for filenotfound exception error

--- project.java
+++ project.java
@@ -60,42 +60,44 @@ public class project {
     }
     public void test(){
         //getting product and putting it into array
-        
-        String infile = "src/main/Java/project1/products.txt"; //CHANGE THIS FOR INPUT TO WORK
-        Scanner rd = new Scanner(infile);
-        Product[] products = new Product[5];
-        int i = 0;
-        while(rd.hasNext()) {
-            String[] buf = rd.nextLine().split(",");
-            products[i] = new Product(buf[0].trim(),
-                                      Integer.parseInt(buf[1].trim()),
-                                      Integer.parseInt(buf[2].trim())
-            );
-            i++;
-        }
-        rd.close();
-        
-        //reading order
-        
-        infile = "src/main/Java/project1/orders.txt"; //CHANGE THIS FOR INPUT TO WORK
-        rd = new Scanner(infile);
-        int[] orderPrice = new int[5];
-        List<Order> orders = new ArrayList<Order>(); //USE ARRAYLIST
-        int count = 0;
-        while(rd.hasNext()){ //read each line from order.txt
-            String[] buf = rd.nextLine().split(",");
-            orders.add(new Order(buf[1].trim(),
-                                 Integer.parseInt(buf[0].trim()),
-                                 buf[2].trim()
-            ));
-            
-            for(i=0;i<orderPrice.length;i++){  //add up price and  weight
-                orders.get(count).addPrice(Integer.parseInt(buf[3+i]) * products[i].price);
-                orders.get(count).addWeight(Integer.parseInt(buf[3+i]) * products[i].weight);
+        try {
+            String infile = "src/main/Java/project1/products.txt"; //CHANGE THIS FOR INPUT TO WORK
+            Scanner rd = new Scanner(new File(infile));
+            Product[] products = new Product[5];
+            int i = 0;
+            while(rd.hasNext()) {
+                String[] buf = rd.nextLine().split(",");
+                products[i] = new Product(buf[0].trim(),
+                                          Integer.parseInt(buf[1].trim()),
+                                          Integer.parseInt(buf[2].trim())
+                );
+                i++;
             }
-            
-            
-            
+            rd.close();
+
+            //reading order
+
+            infile = "src/main/Java/project1/orders.txt"; //CHANGE THIS FOR INPUT TO WORK
+            rd = new Scanner(new File(infile));
+            int[] orderPrice = new int[5];
+            List<Order> orders = new ArrayList<Order>(); //USE ARRAYLIST
+            int count = 0;
+            while(rd.hasNext()){ //read each line from order.txt
+                String[] buf = rd.nextLine().split(",");
+                orders.add(new Order(buf[1].trim(),
+                                     Integer.parseInt(buf[0].trim()),
+                                     buf[2].trim()
+                ));
+
+                for(i=0;i<orderPrice.length;i++){  //add up price and  weight
+                    orders.get(count).addPrice(Integer.parseInt(buf[3+i]) * products[i].price);
+                    orders.get(count).addWeight(Integer.parseInt(buf[3+i]) * products[i].weight);
+                }
+
+
+            }
+        } catch(Exception e){
+            System.err.println(e);
         }
         
     
